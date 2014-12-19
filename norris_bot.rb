af41d93f9f1b5844649a7c0fdefb49cff72745a1
reppard <reppardwalker@gmail.com>
2014-12-19 19:33:38 +0000

    sys tools

--- norris_bot.rb
+++ norris_bot.rb
@@ -22,23 +22,51 @@ def pong(msg)
   @socket.send("PONG\r\n",0)
 end
 
-def yomama
-  joke = JSON.parse(`curl -s http://api.yomomma.info`)["joke"]
-  $stdout.print(joke + "\n")
-  send_privmsg(joke)
-end
-
 def norris
   joke = JSON.parse(`curl -s http://api.icndb.com/jokes/random`)["value"]["joke"]
   $stdout.print(joke + "\n")
   send_privmsg(joke)
 end
 
+def startup
+  idea = JSON.parse(`curl -s http://itsthisforthat.com/api.php?json`)
+  $stdout.print("#{idea.to_s}\n")
+  send_privmsg("Startup Idea: #{idea['this']} for #{idea['that']}")
+end
+
+def disk_u
+  title,du = `df -h | head -n2`.split("\n")
+  send_privmsg(title)
+  send_privmsg(du)
+end
+
+def vmstat
+  stats = `vmstat`.split("\n")
+  stats.each do |line|
+    send_privmsg(line)
+  end
+end
+
+def top(line)
+  top_count = line.split(" ").last.to_i
+  if top_count <= 5
+    lines = `top -n 1 -b | grep -A#{top_count} PID`.split("\n")
+    lines.each do |line|
+      send_privmsg(line)
+    end
+  else
+    send_privmsg("Don't spam the top.")
+  end
+end
+
 def process_line(line)
   join_channel(@channel) if line =~ /MOTD/
   pong(line) if line =~ /PING/
-  norris if line =~ /!norris/
-  yomama if line =~ /!yomama/
+  norris if line  =~ /!norris/
+  startup if line =~ /!startup/
+  disk_u  if line =~ /!du/
+  vmstat  if line =~ /!vm/
+  top(line) if line =~ /!top \d/
 end
 
 def set_options args
@@ -56,7 +84,6 @@ $stdout.sync = true
 @socket = TCPSocket.open(@server, 6667)
 @socket.send("NICK #{@nick}\r\n",0)
 @socket.send("USER rubot * * : rubybottest Test User\r\n",0)
-
 
 while true
   line = @socket.gets
